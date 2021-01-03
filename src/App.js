e02bd0a202303ca0b42a49c0ce32ce0292066d32
Junhua <wenjh1998@gmail.com>
2021-01-03 21:13:56 +0800

    Add structure for pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from "react"
 import './App.css';
+import Header from './components/header'
+import About from './components/about'
+import Education from './components/education'
+import Experience from './components/experience'
+import Footer from './components/footer'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Resume Website">
+      <Header />
+      <About />
+      <Experience />
+      <Education />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/experience.jsx
@@ -0,0 +1,8 @@
+import React, {Component} from 'react'
+
+export default class Experience extends Component {
+
+    render() {
+        return <div>Experience</div>
+    }
+}
