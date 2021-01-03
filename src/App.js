e3ca61769fb19da691a5c1e8424cac5bbaf1dfd9
Junhua <wenjh1998@gmail.com>
2021-01-03 22:11:32 +0800

    Update headers

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import About from './components/about'
 import Education from './components/education'
 import Experience from './components/experience'
 import Footer from './components/footer'
-
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,19 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import { Navbar, Nav } from "react-bootstrap"
 
 export default class Header extends Component {
 
     render() {
-        return <div>Header</div>
+        return (
+        <Navbar class="header" bg="dark" variant="dark">
+            <Navbar.Brand href="#">Junhua</Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link href="#header">Home</Nav.Link>
+                    <Nav.Link href="#about">About</Nav.Link>
+                    <Nav.Link href="#education">Education</Nav.Link>
+                    <Nav.Link href="#experience">Experience</Nav.Link>
+            </Nav>
+        </Navbar>
+        );
     }
 }
